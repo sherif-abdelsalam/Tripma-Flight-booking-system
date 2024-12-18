0e0c87381b2a55aa3e80d2352b32659ecfaf1eca
Sherif Hodhod <sherif.abdelsalam.111@gmail.com>
2024-12-18 18:46:42 +0200

    added more icons

---
+++ components/Icons/arrow->.js
@@ -0,0 +1,19 @@
+export default function Arrow() {
+  return (
+    <svg
+      width="22"
+      height="17"
+      viewBox="0 0 22 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 8.5H20.5M20.5 8.5L13 1M20.5 8.5L13 16"
+        stroke="#A1B0CC"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ components/Icons/person.js
@@ -0,0 +1,17 @@
+export default function PersonIcon() {
+  return (
+    <svg
+      width="16"
+      height="18"
+      viewBox="0 0 16 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="8" cy="4.5" r="4" fill="#6E7491" />
+      <path
+        d="M2 17.5C0.895431 17.5 -0.0256538 16.5907 0.246855 15.5202C1.12788 12.0595 4.26504 9.5 8 9.5C11.735 9.5 14.8721 12.0595 15.7531 15.5202C16.0257 16.5907 15.1046 17.5 14 17.5H2Z"
+        fill="#6E7491"
+      />
+    </svg>
+  );
+}

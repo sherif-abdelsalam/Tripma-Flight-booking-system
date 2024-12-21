9b927adab9676305787bee5797db97358821f906
Sherif Hodhod <sherif.abdelsalam.111@gmail.com>
2024-12-22 00:18:41 +0200

    updated icons

---
+++ components/Icons/left-arrow.js
@@ -0,0 +1,19 @@
+export default function LeftArrow() {
+  return (
+    <svg
+      width="10"
+      height="18"
+      viewBox="0 0 10 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 1.5L1.5 9L9 16.5"
+        stroke="#6E7491"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ components/Icons/right-arrow.js
@@ -0,0 +1,19 @@
+export default function RightArrow() {
+  return (
+    <svg
+      width="10"
+      height="18"
+      viewBox="0 0 10 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 1.5L8.5 9L1 16.5"
+        stroke="#6E7491"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}

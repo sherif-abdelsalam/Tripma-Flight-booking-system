085fe9bf5c0c329f3ddfaa6d60f7c71fb402d913
Sherif Hodhod <sherif.abdelsalam.111@gmail.com>
2024-12-26 12:16:13 +0200

    updated icons

---
+++ components/Icons/arrow.js
@@ -0,0 +1,18 @@
+export default function Arrow() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 16.0005H25.5M25.5 16.0005L18 8.50049M25.5 16.0005L18 23.5005"
+        stroke="white"
+        strokeLidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ components/Icons/avaliable.js
@@ -0,0 +1,33 @@
+export function BusinessAvailable() {
+  return (
+    <svg
+      width="17"
+      height="14"
+      viewBox="0 0 17 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 7.00098L6.24264 11.2436L14.7279 2.75834"
+        stroke="#5CD6C0"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function EconomyAvailable() {
+  return (
+    <svg
+      width="10"
+      height="10"
+      viewBox="0 0 10 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="5" cy="5.00098" r="5" fill="#605DEC" />
+    </svg>
+  );
+}
